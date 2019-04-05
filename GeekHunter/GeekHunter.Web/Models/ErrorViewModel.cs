6eb3c93f8451ef852bd8aca5c4d7c0a4ff007af4
Tom Liu <wuzi.xiao@gmail.com>
2019-04-05 17:11:11 +1300

    Init

---
+++ GeekHunter/GeekHunter.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GeekHunter.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
